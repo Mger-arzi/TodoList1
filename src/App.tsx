145c268a827b447893e3241a3c2d79c2f86fc8e2
Mger-arzi <mger54598@gmail.com>
2023-11-13 19:04:57 +0300

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { TodoList } from './TodoList';
 
 function App() {
 
-    const tasks1 = [
+    // const tasks1 = [
+    //     {id: 1, title: "JS", isDone: true},
+    //     {id: 2, title: "HTML/CSS", isDone: true},
+    //     {id: 3, title: "React", isDone: true}
+    // ]
+    
+
+    const [tasks, setTasks] = useState([
         {id: 1, title: "JS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "JS", isDone: true}
-    ]
-    const tasks2 = [
-        {id: 4, title: "JS", isDone: true},
-        {id: 5, title: "JS", isDone: true},
-        {id: 6, title: "JS", isDone: true}
-    ]
-    const tasks3 = [
-        {id: 7, title: "JS", isDone: true},
-        {id: 8, title: "JS", isDone: true},
-        {id: 9, title: "JS", isDone: true}
-    ]
+        {id: 2, title: "HTML/CSS", isDone: true},
+        {id: 3, title: "React", isDone: true}
+    ])
+
+    const removeTask = () => {
+        return(
+
+        )
+    }
+
     return (
         <div className='App'>
-            <TodoList title='What to learn' tasks={tasks1}/>
-            <TodoList title='What to learn' tasks={tasks2}/>
-            <TodoList title='What to learn' tasks={tasks3}/>
+            <TodoList title='What to learn' tasks={tasks}/>
         </div>
     );
 }
