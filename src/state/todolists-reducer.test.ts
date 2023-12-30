a838fa2da13867d070b8faffbbdd7aebe1924703
Mger-arzi <mger54598@gmail.com>
2023-12-30 11:13:41 +0300

    add

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,7 +1,7 @@
 
 import { v1 } from 'uuid';
 import { TodolistsType } from '../App';
-import { removeTodolistAC, todolistsReducer } from './todolists-reducer';
+import { addTodolistAC, removeTodolistAC, todolistsReducer } from './todolists-reducer';
 
 
 
@@ -20,4 +20,20 @@ test('correct todolist should be removed', ()=>{
 
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todolistID2)
+})
+
+test ('correct todolist should be added', ()=> {
+    let todolistID1 = v1()
+    let todolistID2 = v1()
+
+    const startState: Array<TodolistsType> = [
+        { id: todolistID1, title: 'What to learn', filter: 'All' },
+        { id: todolistID2, title: 'What to buy', filter: 'All' },
+    ]
+    let newTodolistTitle = "New Todolist"
+
+    const endState = todolistsReducer(startState , addTodolistAC(newTodolistTitle))
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
 })
--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,3 +1,4 @@
+import { v1 } from "uuid";
 import { TodolistsType } from "../App";
 
 export const todolistsReducer = (state:TodolistsType[], action: TodolistsReducerType):TodolistsType[]=> {
@@ -8,10 +9,19 @@ export const todolistsReducer = (state:TodolistsType[], action: TodolistsReducer
         case 'REMOVE-TODOLIST': {
             return state.filter(el => el.id !== action.payloard.id)
         }
+        //const newID = v1()
+        //const newTodo: TodolistsType = { id: newID, title: trimedTitle, filter: 'All' }
+        //setTodolists([newTodo, ...todolists])
+        //setTasks({ ...tasks, [newID]: [] })
+        case 'ADD-TODOLIST': {
+        const newID = v1()
+        const newTodo: TodolistsType = { id: newID, title: action.payloard.trimedTitle, filter: 'All' }
+            return [...state , newTodo]
+        }
         default: return state
     }
 }
-type TodolistsReducerType  = RemoveTodolistACType
+type TodolistsReducerType  = RemoveTodolistACType | AddTodolistAC
 
 type RemoveTodolistACType = ReturnType<typeof removeTodolistAC>
 
@@ -19,5 +29,13 @@ export const removeTodolistAC = (id:string) => {
     return{
         type: 'REMOVE-TODOLIST',
         payloard : {id }
+    }as const
+}
+
+type AddTodolistAC = ReturnType<typeof addTodolistAC>
+export const addTodolistAC = (trimedTitle:string) => {
+    return{
+        type: 'ADD-TODOLIST',
+        payloard : { trimedTitle }
     }as const
 }
