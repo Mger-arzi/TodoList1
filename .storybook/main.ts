cc4e5ddfe23a51d81e2a3788a8bc576b256f69fa
Mger-arzi <mger54598@gmail.com>
2024-01-27 17:38:00 +0300

    add 5 snapShot test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,38 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {
-      builder: {
-        useSWC: true,
-      },
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-interactions",
+        {
+            name: "@storybook/addon-storysource",
+        options:{
+            rule:{
+                test: [/\.stories\.tsx?$/],
+            },
+            loaderOptionns:{
+                prettierConfig:{
+                    printWidth: 80,singleQuote:false,
+                    options:{parent:"typescript"}
+                }
+            }
+        }}
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {
+            builder: {
+                useSWC: true,
+            },
+        },
     },
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  staticDirs: ["..\\public"],
+    docs: {
+        autodocs: "tag",
+    },
+    staticDirs: ["..\\public"],
 };
 export default config;
