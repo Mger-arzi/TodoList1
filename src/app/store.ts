19a48f926632f87c4fb4071261d2039a894fbdbd
Mger-arzi <mger54598@gmail.com>
2024-03-31 20:16:15 +0300

    add

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,16 @@
-import { tasksReducer } from '../components/tasks/tasks-reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector, } from 'react-redux';
+import { AnyAction, applyMiddleware, combineReducers, legacy_createStore, } from 'redux';
+import { ThunkDispatch, thunk } from 'redux-thunk';
 import { todolistsReducer } from '../components/features/TodolistList/todolists-reducer';
-import { AnyAction, applyMiddleware, combineReducers, legacy_createStore } from 'redux';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import  { ThunkDispatch, thunk} from 'redux-thunk';
-import { appReducer } from './app-reducer';
 import { authReducer } from '../components/features/login/auth-reducer';
+import { tasksReducer } from '../components/tasks/tasks-reducer';
+import { appReducer } from './app-reducer';
 
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
+
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
@@ -18,6 +20,11 @@ const rootReducer = combineReducers({
 // непосредственно создаём store
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+// export const store = configureStore({
+//     reducer: rootReducer,
+//     middleware : getDefaultMiddleware => getDefaultMiddleware().prepend(thunk)
+// });
+
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
